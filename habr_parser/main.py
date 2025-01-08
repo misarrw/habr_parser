0467d03f9226048003462b902e16d24a869db30a
misarrw <sk074866336@gmail.com>
2025-01-08 18:03:04 +0300

    функционал полностью реализован

--- habr_parser/main.py
+++ habr_parser/main.py
@@ -12,20 +12,23 @@ def find_by_key(word):
     response = requests.get(search_url)
     soup = BeautifulSoup(response.text, 'lxml')
     articles = soup.find_all('article', class_='tm-articles-list__item')
-    key_articles = []
+    key_articles_dict = {}
+    key_articles_list = []
     
     for article in articles:
         title_block = article.find('h2', class_='tm-title tm-title_h2')
         if title_block and title_block.find('span'):
+            link_tag = title_block.find('a', class_='tm-title__link')
+            link = link = 'https://habr.com' + link_tag['href']
             title = title_block.find('span').text
-            key_articles.append(title)
-    
+            key_articles_dict[title] = link
+            key_articles_list.append(title)
     habr = 'https://habr.com/ru/search/'
-    return key_articles
+    return key_articles_dict, key_articles_list
 
 class ParseTheArticle():
     def find_all_properties(self):
-        response = requests.get(link).text
+        response = requests.get(self.url).text
         soup = BeautifulSoup(response, 'lxml')
 
         title_block = soup.find('h1')
@@ -40,11 +43,10 @@ class ParseTheArticle():
 
 
 class Article(ParseTheArticle):
-
-    def __init__(self):
+    def __init__(self, url):
+        self.url = url
         article = self.find_all_properties()
         self.title = article[0]
-        self.url = link
         self.author = article[1]
         self.contents = article[2]
 
@@ -67,16 +69,23 @@ class Article(ParseTheArticle):
 
 def show_page():
     global page
-    result = find_by_key(word)
-    for number, article in enumerate(result):
-        print(f'{number + 1}. {article}')
+    key_articles = find_by_key(word)
+    key_articles_list = key_articles[1]
+    key_articles_dict = key_articles[0]
+    for index, article in enumerate(key_articles_list):
+        print(f'{index + 1}. {article}')
     print('If you wanna see other articles, write "next"\nOr write the number of the article you wanna see:')
     next_or_number = input()
     if next_or_number == 'next':
         page += 1
         show_page()
-    else:
-        print(result[int(next_or_number) - 1])
+    elif next_or_number.isdigit() == True and int(next_or_number) in range(21):
+        element = key_articles_list[int(next_or_number) - 1]
+        url = key_articles_dict[element]
+        article_url = Article(url)
+        article = article_url.convert_to_dict()
+        with open('result.json', 'w', encoding='utf-8') as json_file:
+            json.dump({'data' : article}, json_file, ensure_ascii=False, indent=4)
 
 
 page = 1
@@ -84,9 +93,9 @@ print('Write the word "key" if you wanna search articles by the key word\nWrite 
 paste = input()
 if paste == 'link':
     print('Paste the link:')
-    link = input()
-    link = Article()
-    article = link.convert_to_dict()
+    article_url = input()
+    article_url = Article(article_url)
+    article = article_url.convert_to_dict()
     with open('result.json', 'w', encoding='utf-8') as json_file:
             json.dump({'data' : article}, json_file, ensure_ascii=False, indent=4)
 elif paste == 'key':
